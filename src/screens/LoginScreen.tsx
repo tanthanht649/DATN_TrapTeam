2414dafe354011ba9e39144f47dbad729f97d190
tanthanht649 <98208915+tanthanht649@users.noreply.github.com>
2023-09-13 20:12:18 +0700

    bosung

---
+++ src/screens/LoginScreen.tsx
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const LoginScreen = () => {
+    return (
+        <View>
+            <Text>LoginScreen</Text>
+            <Text>
+                KKKSS
+            </Text>
+        </View>
+    )
+}
+
+export default LoginScreen
+
+const styles = StyleSheet.create({
+
+})
