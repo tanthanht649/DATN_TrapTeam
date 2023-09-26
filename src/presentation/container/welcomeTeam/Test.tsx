82d9bafe25f0dcf9e3e854f90960a265bb4a6b00
luvantrong <102342647+luvantrong@users.noreply.github.com>
2023-09-26 11:43:29 +0700

    Update Test.tsx

--- src/presentation/container/welcomeTeam/Test.tsx
+++ src/presentation/container/welcomeTeam/Test.tsx
@@ -1,12 +1,8 @@
 import {Pressable, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {SafeAreaView} from 'react-native-safe-area-context';
-
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {WelcomeTeamStackParamList} from '@navigation';
-import {ItemLocation} from '@components';
-import {LOCATION, LOCATION_2, LOCATION_3} from '@assets';
-import {Colors} from '@resources';
 
 type PropsType = NativeStackScreenProps<WelcomeTeamStackParamList, 'Test'>;
 const _Test: React.FC<PropsType> = props => {
